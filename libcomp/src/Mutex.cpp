43edeed0c61b1027dd3d7c683009f46c120680d6
COMP Omega <compomega@tutanota.com>
2019-05-27 12:59:45 -0600

    [ADD] Mutex wrapper to detect errors on Linux.

---
+++ libcomp/src/Mutex.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file libcomp/src/Mutex.cpp
+ * @ingroup libcomp
+ *
+ * @author COMP Omega <compomega@tutanota.com>
+ *
+ * @brief Implementation of the Mutex class.
+ *
+ * This file is part of the COMP_hack Library (libcomp).
+ *
+ * Copyright (C) 2012-2019 COMP_hack Team <compomega@tutanota.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Mutex.h"
+
+using namespace libcomp;
+
+Mutex::Mutex() : mLocked(false)
+{
+}
+
+void Mutex::lock()
+{
+    mMutex.lock();
+
+    if(mLocked)
+    {
+        throw std::system_error(EDEADLK, std::generic_category(),
+            "double lock detected");
+    }
+
+    mLocked = true;
+}
+
+void Mutex::unlock()
+{
+    if(!mLocked)
+    {
+        throw std::system_error(EDEADLK, std::generic_category(),
+            "double unlock detected");
+    }
+
+    mLocked = false;
+
+    mMutex.unlock();
+}
+
+bool Mutex::try_lock()
+{
+    bool didLock = mMutex.try_lock();
+
+    if(didLock)
+    {
+        if(mLocked)
+        {
+            throw std::system_error(EDEADLK, std::generic_category(),
+                "double lock detected");
+        }
+
+        mLocked = true;
+    }
+
+    return didLock;
+}
---
+++ libcomp/src/Mutex.h
@@ -0,0 +1,79 @@
+/**
+ * @file libcomp/src/Mutex.h
+ * @ingroup libcomp
+ *
+ * @author COMP Omega <compomega@tutanota.com>
+ *
+ * @brief Definition of the Mutex class.
+ *
+ * This file is part of the COMP_hack Library (libcomp).
+ *
+ * Copyright (C) 2012-2019 COMP_hack Team <compomega@tutanota.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBCOMP_SRC_MUTEX_H
+#define LIBCOMP_SRC_MUTEX_H
+
+// Standard C++11 Includes
+#include <mutex>
+
+namespace libcomp
+{
+
+/**
+ * Mutex wrapper that checks if the mutex has already been locked. If the
+ * mutex has already been locked an exception will be thrown.
+ */
+class Mutex
+{
+public:
+    /**
+     * Construct a new mutex.
+     */
+    Mutex();
+
+    /**
+     * You may not copy a mutex.
+     */
+    Mutex(const Mutex&) = delete;
+
+    /**
+     * Block until the mutex can be locked by this thread.
+     */
+    void lock();
+
+    /**
+     * Unlock the mutex.
+     */
+    void unlock();
+
+    /**
+     * Attempt to lock this mutex but do not block.
+     * @returns true if the mutex is locked; false otherwise.
+     */
+    bool try_lock();
+
+private:
+    /// If the mutex has already been locked.
+    bool mLocked;
+
+    /// Underlying mutex.
+    std::recursive_mutex mMutex;
+};
+
+} // namespace libcomp
+
+#endif // LIBCOMP_SRC_MUTEX_H
